de915fb00087f166938473f59066672c23943762
montonurb <brunomoncar007@gmail.com>
2022-09-09 11:49:58 -0300

    repeticao com for

---
+++ repeticao/src/repeticao/RepeticaoFor.java
@@ -0,0 +1,11 @@
+package repeticao;
+
+public class RepeticaoFor {
+    public static void main(String[] args) {
+        int valor = 0;
+        for(int contador = 0; contador <= 10; contador++) {
+            valor += contador;
+        }
+        System.out.println(valor);
+    }
+}
