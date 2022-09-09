7e221545433639eccde5ba7c9b29af40dd81d2a4
montonurb <brunomoncar007@gmail.com>
2022-09-09 11:44:57 -0300

    repeticao com while

---
+++ repeticao/src/repeticao/RepeticaoWhile.java
@@ -0,0 +1,14 @@
+package repeticao;
+
+public class RepeticaoWhile {
+
+    public static void main(String[] args) {
+        int contador = 0;
+        
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+    
+}
