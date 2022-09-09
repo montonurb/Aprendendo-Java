4d3f5b154b13e959000d8bb1574a0bb01a96ce81
montonurb <brunomoncar007@gmail.com>
2022-09-09 12:55:45 -0300

    repetições encadeadas

---
+++ repeticao/src/repeticao/RepeticaoEncadeada.java
@@ -0,0 +1,13 @@
+
+package repeticao;
+
+public class RepeticaoEncadeada {
+    public static void main(String[] args) {
+        for(int linha = 1; linha <= 5; linha++){
+            for(int coluna = 1; coluna <= 5; coluna++){
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
