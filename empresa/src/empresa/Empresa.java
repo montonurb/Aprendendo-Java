704ed80543f40289c532b968dfadf528fa11aee7
montonurb <brunomoncar007@gmail.com>
2022-09-12 14:53:50 -0300

    Herança e construtores

--- empresa/src/empresa/Empresa.java
+++ empresa/src/empresa/Empresa.java
@@ -3,8 +3,7 @@ package empresa;
 public class Empresa {
 
     public static void main(String[] args) {
-        Gerente bruno = new Gerente();
-        bruno.setNome("Bruno Monteiro");
+        Gerente bruno = new Gerente("Bruno Monteiro");
         bruno.setSalario(2500);
         
         System.out.println("Bonificacao: " + bruno.bonificacao());
