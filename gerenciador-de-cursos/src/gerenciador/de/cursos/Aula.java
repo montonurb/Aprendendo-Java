5b9c0739f12429280e25e8d32491677ac611f116
montonurb <brunomoncar007@gmail.com>
2022-09-14 08:29:55 -0300

    criando a classe aula e ordenando por atributo

---
+++ gerenciador-de-cursos/src/gerenciador/de/cursos/Aula.java
@@ -0,0 +1,26 @@
+package gerenciador.de.cursos;
+
+public class Aula {
+    private String titulo;
+    private int tempo;
+    
+    public Aula(String titulo, int  tempo){
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public int getTempo() {
+        return tempo;
+    }
+
+    @Override
+    public String toString() {
+        return this.titulo + " - " + this.tempo + " minutos";
+    }
+    
+    
+}
--- gerenciador-de-cursos/src/gerenciador/de/cursos/GerenciadorDeCursos.java
+++ gerenciador-de-cursos/src/gerenciador/de/cursos/GerenciadorDeCursos.java
@@ -2,11 +2,13 @@ package gerenciador.de.cursos;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GerenciadorDeCursos {
 
     public static void main(String[] args) {
-
+        /*  PRIMEIRA AULA
         String aula1 = "Conhecendo mais de listas";
         String aula2 = "Modelando a classe Aula";
         String aula3 = "Trabalhando com cursos e sets";
@@ -28,6 +30,25 @@ public class GerenciadorDeCursos {
         }
         
         aulas.add("Aumentando nosso conhecimento");
+        System.out.println(aulas);
+         */
+
+        Aula aula1 = new Aula("Trabalhando com ArrayList", 24);
+        Aula aula2 = new Aula("Lista de objetos", 19);
+        Aula aula3 = new Aula("Relacionamentos com colecoes", 15);
+
+        ArrayList<Aula> aulas = new ArrayList<>();
+
+        aulas.add(aula1);
+        aulas.add(aula2);
+        aulas.add(aula3);
+
+        System.out.println(aulas);
+        
+        Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+        System.out.println(aulas);
+        
+        Collections.sort(aulas, Comparator.comparing(Aula::getTitulo));
         System.out.println(aulas);
     }
 
