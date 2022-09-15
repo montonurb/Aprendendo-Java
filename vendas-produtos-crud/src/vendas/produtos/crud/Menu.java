d6e02a18f00a54c68ef1db28ac184d7f1c00eba2
montonurb <brunomoncar007@gmail.com>
2022-09-15 19:26:15 -0300

    organizando menu

--- vendas-produtos-crud/src/vendas/produtos/crud/Menu.java
+++ vendas-produtos-crud/src/vendas/produtos/crud/Menu.java
@@ -3,8 +3,32 @@ package vendas.produtos.crud;
 import java.util.Scanner;
 
 public class Menu {
+    
+    public void menuDeOpcoes() {
+        Scanner entrada = new Scanner(System.in);
+        int opcao = 0;
+
+        System.out.println("0 - Sair");
+        System.out.println("1 - Realizar venda");
+        System.out.println("2 - Ver carrinho");
+        System.out.println("3 - Finalizar venda");
+        System.out.println("4 - Cadastrar produto");
+        System.out.println("5 - Cadastrar funcionario");
+        System.out.println("6 - Cadastrar cliente");
+
+        System.out.print("Opcao: ");
+        
+        try {
+            opcao = Integer.parseInt(entrada.next());
+            escolhaDoUsuario(opcao);
+        } catch(Exception e) {
+            System.out.println(e + " nao e um numero inteiro.");
+        }
+    }
 
     public void escolhaDoUsuario(int opcao) {
+        
+        System.out.println("ENTROU");
 
         switch (opcao) {
             case 0:
@@ -55,6 +79,7 @@ public class Menu {
                 break;
             default:
                 System.out.println("opcao invalida!");
+                menuDeOpcoes();
                 break;
         }
     }
--- vendas-produtos-crud/src/vendas/produtos/crud/main.java
+++ vendas-produtos-crud/src/vendas/produtos/crud/main.java
@@ -5,26 +5,7 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int opcao = 0;
-
-        System.out.println("0 - Sair");
-        System.out.println("1 - Realizar venda");
-        System.out.println("2 - Ver carrinho");
-        System.out.println("3 - Finalizar venda");
-        System.out.println("4 - Cadastrar produto");
-        System.out.println("5 - Cadastrar funcionario");
-        System.out.println("6 - Cadastrar cliente");
-
-        System.out.print("Opcao: ");
-        
-        try {
-            opcao = Integer.parseInt(entrada.next());
-        } catch(Exception e) {
-            System.out.println(e + " nao e um numero inteiro.");
-        }
-        
         Menu menu = new Menu();
-        menu.escolhaDoUsuario(opcao);
+        menu.menuDeOpcoes();
     }
 }
