300cc5b839e34e93ce7b31a2931863d235ccd80e
montonurb <brunomoncar007@gmail.com>
2022-09-16 10:26:17 -0300

    ajustando variaveis da classe Venda

--- vendas-produtos-crud/src/vendas/produtos/crud/Venda.java
+++ vendas-produtos-crud/src/vendas/produtos/crud/Venda.java
@@ -5,11 +5,9 @@ import java.util.Scanner;
 
 public class Venda {
 
-    int id;
     private int numeroDaVenda;
     private Funcionario funcionarioVenda;
     private Cliente clienteVenda;
     private List<Produto> produtos;
 
-    
 }
