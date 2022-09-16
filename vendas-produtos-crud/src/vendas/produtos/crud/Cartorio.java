7f5ec2787048bb7c3ffd4fd389b04e5f1d0b60d1
montonurb <brunomoncar007@gmail.com>
2022-09-16 10:22:26 -0300

    criando metodo de cadastrar funcionarios e ajustando variaveis

--- vendas-produtos-crud/src/vendas/produtos/crud/Cartorio.java
+++ vendas-produtos-crud/src/vendas/produtos/crud/Cartorio.java
@@ -1,7 +1,13 @@
 package vendas.produtos.crud;
 
+import java.util.ArrayList;
+
 public class Cartorio {
-    private int id;
     private String nome;
     private String endereco;
+    ArrayList<Funcionario> funcionarios = new ArrayList<>();
+    
+    public void cadastrarFuncionarios(String nome, String funcao) {
+        funcionarios.add(new Funcionario(nome, funcao));
+    }
 }
