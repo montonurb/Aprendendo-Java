ffae9243ea0896e5848b062d61a1282a92fe44a3
montonurb <brunomoncar007@gmail.com>
2022-12-07 20:29:27 -0300

    Ajustando as Classes

--- fox-fifa/src/main/java/model/Time.java
+++ fox-fifa/src/main/java/model/Time.java
@@ -8,9 +8,51 @@ import java.util.List;
  * @author montonurb
  */
 public class Time {
-    Long id;
-    String nome;
-    int capacidade;
-    List<Object> jogadores;
-    Date dataCadastro;
+    private Long id;
+    private String nome;
+    private int capacidade;
+    private List<Object> jogadores;
+    private Date dataCadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public List<Object> getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(List<Object> jogadores) {
+        this.jogadores = jogadores;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    
+    
 }
